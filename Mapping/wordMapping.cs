f78e6926fb5e832488180028bb4e383c951340ed
Sopheak <kosopheakko@gmail.com>
2024-07-21 09:00:32 -0700

    chnage date to pacific standard time

--- Mapping/wordMapping.cs
+++ Mapping/wordMapping.cs
@@ -1,48 +1,62 @@
+using System;
 using upword.Api.Dtos;
 using upword.Api.Entities;
 
-namespace upword.Api.Mapping;
-
-public static class WordMapping
+namespace upword.Api.Mapping
 {
-    public static Word ToEntity(this CreateWordDto wordDto, string newId)
+    public static class WordMapping
     {
-        return new Word
+        public static Word ToEntity(this CreateWordDto wordDto, string newId)
         {
-            Id = newId,
-            Value = wordDto.Value, // Changed from 'word' to 'Value'
-            Definition = wordDto.Definition,
-            PartOfSpeech = wordDto.PartOfSpeech,
-            Pronunciation = wordDto.Pronunciation,
-            ExampleSentences = wordDto.ExampleSentences, // Change to array of strings
-            DateAdded = DateOnly.FromDateTime(DateTime.UtcNow)
-        };
-    }
+            return new Word
+            {
+                Id = newId,
+                Value = wordDto.Value, // Changed from 'word' to 'Value'
+                Definition = wordDto.Definition,
+                PartOfSpeech = wordDto.PartOfSpeech,
+                Pronunciation = wordDto.Pronunciation,
+                ExampleSentences = wordDto.ExampleSentences, // Change to array of strings
+                DateAdded = GetPacificStandardTimeDate()
+            };
+        }
 
-    public static WordDto ToDto(this Word word) // Change to ToDto
-    {
-        return new WordDto(
-            word.Id,
-            word.Value,
-            word.Definition,
-            word.PartOfSpeech,
-            word.Pronunciation,
-            word.ExampleSentences,
-            word.DateAdded
-        );
-    }
-
-    public static Word ToEntity(this UpdateWordDto wordDto, string id)
-    {
-        return new Word
+        public static WordDto ToDto(this Word word) // Change to ToDto
         {
-            Id = id,
-            Value = wordDto.Value,
-            Definition = wordDto.Definition,
-            PartOfSpeech = wordDto.PartOfSpeech,
-            Pronunciation = wordDto.Pronunciation,
-            ExampleSentences = wordDto.ExampleSentences,
-            DateAdded = wordDto.DateAdded
-        };
+            return new WordDto(
+                word.Id,
+                word.Value,
+                word.Definition,
+                word.PartOfSpeech,
+                word.Pronunciation,
+                word.ExampleSentences,
+                word.DateAdded
+            );
+        }
+
+        public static Word ToEntity(this UpdateWordDto wordDto, string id)
+        {
+            return new Word
+            {
+                Id = id,
+                Value = wordDto.Value,
+                Definition = wordDto.Definition,
+                PartOfSpeech = wordDto.PartOfSpeech,
+                Pronunciation = wordDto.Pronunciation,
+                ExampleSentences = wordDto.ExampleSentences,
+                DateAdded = wordDto.DateAdded
+            };
+        }
+
+        private static DateOnly GetPacificStandardTimeDate()
+        {
+            // Define Pacific Standard Time (PST) timezone
+            TimeZoneInfo pacificZone = TimeZoneInfo.FindSystemTimeZoneById("Pacific Standard Time");
+
+            // Convert current UTC time to PST
+            DateTime pacificTime = TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, pacificZone);
+
+            // Return only the date part
+            return DateOnly.FromDateTime(pacificTime);
+        }
     }
 }
